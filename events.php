cbcb18d825b13b09dae5dd87cb7ccaaf29526737
Ziaa <s.zia.azimi@gmail.com>
2015-05-18 21:07:08 +0430

    Add php files

---
+++ events.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap-rtl.css" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap-nav-rtl.css" />
+<link type="text/css" rel="stylesheet" href="css/custom.css" />
+<link type="text/css" rel="stylesheet" href="css/font.css" />
+<link type="text/css" rel="stylesheet" href="css/style.css" />
+<link type="text/css" rel="stylesheet" href="css/flipclock.css" />
+<title>Hacka{city}</title>
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+      <div class="container">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="./"><img src="assets/logo/logo.png" width="40" /></a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li><a href="./">هاکا</a></li>
+                <li class="active"><a href="events.php">رویدادها</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-left">
+                <li><a href="#" data-toggle="modal" data-target="#sign-up-modal">ثبت نام</a></li>
+                <li><a href="#" data-toggle="modal" data-target="#login-modal">ورود</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
+</header><!-- /header -->
+
+<div class="wrapper">
+
+<section id="events-main" class="events-main-section">
+
+</section><!-- /.main -->
+
+
+
+<section id="events" class="events-section">
+	<div class="container">
+    
+        <div class="row item">
+            <div class="col-xs-12 col-sm-7 rtl">
+                <h2>رویداد هاکا گلوبال اراک</h2>
+                <p class="text-justify">جامعۀ برنامه نویسان در دنیا خیلی فعال هستند و با اشتراک گذاری دانش میان یکدیگر باعث رشد و پیشرفت جامعۀ برنامه نویسی می شوند. در ایران کمی اوضاع فرق می کند و برنامه نویسان معمولاً زیاد از فعالیت های یکدیگر اطلاعی ندارند و در اتاق خودشان مشغول کدنویسی هستند. این رویداد باعث می شود که اولاً با همکاران خودشان بیشتر آشنا شوند </p>
+                <div class="clock1"></div>
+            </div>
+            <div class="col-xs-12 col-sm-5">
+                <img class="img-responsive img-thumbnail img-rounded center-block" src="assets/banner/1.jpg" />
+            </div>
+        </div>
+     
+     
+        <div class="row item">
+      	  <div class="col-xs-12 col-sm-5">
+                <img class="img-responsive img-thumbnail img-rounded center-block" src="assets/banner/2.jpg" />
+            </div>
+            <div class="col-xs-12 col-sm-7 rtl">
+                <h2>رویداد هاکا گلوبال تهران</h2>
+                <p class="text-justify">جامعۀ برنامه نویسان در دنیا خیلی فعال هستند و با اشتراک گذاری دانش میان یکدیگر باعث رشد و پیشرفت جامعۀ برنامه نویسی می شوند. در ایران کمی اوضاع فرق می کند و برنامه نویسان معمولاً زیاد از فعالیت های یکدیگر.</p>
+                <div class="clock2"></div>
+            </div>
+        </div>
+     
+     
+        <div class="row item">
+            <div class="col-xs-12 col-sm-7 rtl">
+                <h2>رویداد هاکا گلوبال مشهد</h2>
+                <p class="text-justify">سان معمولاً زیاد از فعالیت های یکدیگر اطلاعی ندارند و در اتاق خودشان مشغول کدنویسی هستند. این رویداد باعث می شود که اولاً با همکاران خودشان بیشتر آشنا شوند و شبکه سازی در میان آنان به صورت حضوری و مجازی انجام شود، دوم اینکه شرکت هایی که به دنبال نیروهای متخصص می گردند، با حمایت مالی از رویداد می توانند برنامه نویسان خوب و خبره را در این رویدادها شناسایی و جذب نمایند.</p>
+                <div class="clock3"></div>
+            </div>
+            <div class="col-xs-12 col-sm-5">
+                <img class="img-responsive img-thumbnail img-rounded center-block" src="assets/banner/3.jpg" />
+            </div>
+        </div>
+            
+    </div>
+</section><!-- /.events -->
+
+
+<section class="map-section">
+	<div id="map"></div><!--//map-->
+</section><!--//map-->
+
+
+
+
+<section id="support" class="section-support">
+	<h1 id="title" class="text-center">حامیان  ما</h1>
+    <div class="container text-center">
+    	<img src="assets/support/iea.png" />
+    	<img src="assets/support/iwf.png" />
+    	<img src="assets/support/roshanshow.png" />
+    	<img src="assets/support/startupweekend.png" />
+    </div>
+</section>
+
+
+
+
+<section id="footer" class="section-footer">
+	<div class="container">
+        <div class="row">
+        	<div class="col-xs-4 text-center">
+            	<img src="assets/logo/ahora_logo.png" width="100" />
+            </div>
+            <div class="col-xs-4">
+            	<p class="text-center">تمامی حقوق این سایت متعلق به هاکا سیتی می باشد</p>
+            	<p class="text-center">طراحی و توسعه توسط گروه اهورا</p>
+            </div>
+            <div class="col-xs-4 text-center">
+            	<img src="assets/images/logo_hacka_final.png" width="100" />
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<div class="modal fade" id="sign-up-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-right">ثبت نام</h4>
+      </div>
+      <div class="modal-body rtl">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label for="full_name" class="col-sm-3 control-label">نام و نام خانوادگی </label>
+            <div class="col-sm-9">
+              <input type="text" class="form-control" id="full_name" placeholder="نام و نام خانوادگی">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="email" class="col-sm-3 control-label">ایمیل</label>
+            <div class="col-sm-9">
+              <input type="email" class="form-control" id="email" placeholder="ایمیل">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="pass" class="col-sm-3 control-label">رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="password" class="form-control" id="pass" placeholder="رمز ورود">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="re-pass" class="col-sm-3 control-label">تکرار رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="re-pass" class="form-control" id="re-pass" placeholder="تکرار رمز ورود">
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer rtl">
+        <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
+        <button type="button" class="btn btn-success">ثبت نام</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<div class="modal fade" id="login-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-right">ورود </h4>
+      </div>
+      <div class="modal-body rtl">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label for="email" class="col-sm-3 control-label">ایمیل</label>
+            <div class="col-sm-9">
+              <input type="email" class="form-control" id="email" placeholder="ایمیل">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="pass" class="col-sm-3 control-label">رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="password" class="form-control" id="pass" placeholder="رمز ورود">
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer rtl">
+        <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
+        <button type="button" class="btn btn-primary">ورود </button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+</body>
+
+
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="js/respond.js"></script>
+    <script type="text/javascript" src="js/parallax.js"></script>
+	<script type="text/javascript" src="js/flipclock.js"></script>
+    <script type="text/javascript">
+		var clock;
+		
+		$(document).ready(function() {
+			
+			var clock1;
+			clock1 = $('.clock1').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+				language: 'ar',
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });    
+		    clock1.setTime(1320880);
+		    clock1.setCountdown(true);
+		    clock1.start();
+			
+			
+			var clock2;
+			clock2 = $('.clock2').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+				language: 'ar',
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });
+				    
+		    clock2.setTime(1250880);
+		    clock2.setCountdown(true);
+		    clock2.start();
+			
+			
+			var clock3;
+			clock3 = $('.clock3').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+				language: 'ar',
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });
+				    
+		    clock3.setTime(1527880);
+		    clock3.setCountdown(true);
+		    clock3.start();
+
+		});
+	</script>
+    <!-- Map -->
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script type="text/javascript" src="js/plugins/gmaps.js"></script>  
+    <script type="text/javascript" src="js/plugins/map.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap-rtl.css" />
+<link type="text/css" rel="stylesheet" href="css/bootstrap-nav-rtl.css" />
+<link type="text/css" rel="stylesheet" href="css/custom.css" />
+<link type="text/css" rel="stylesheet" href="css/font.css" />
+<link type="text/css" rel="stylesheet" href="css/style.css" />
+<link type="text/css" rel="stylesheet" href="css/flipclock.css" />
+
+<style type="text/css">
+body{
+	margin-top:40px;
+}
+</style>
+<title>Hacka{city}</title>
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+      <div class="container">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="./"><img src="assets/logo/logo.png" width="40" /></a>
+            </div>
+        
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="./">هاکا</a></li>
+                <li><a href="events.php">رویدادها</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-left">
+                <li><a href="#" data-toggle="modal" data-target="#sign-up-modal">ثبت نام</a></li>
+                <li><a href="#" data-toggle="modal" data-target="#login-modal">ورود</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
+</header><!-- /header -->
+
+<div class="wrapper">
+
+<section id="main" class="main-section">
+	<div id="hacka-bg" class="hacka-bg">
+      
+    </div>
+</section><!-- /.main -->
+
+<section id="about" class="about-section">
+	<h1 id="title" class="text-center"><img src="assets/logo/logo-dark.png" /> معرفی هاکا </h1>
+    <div class="container text-right rtl">
+    	<div class="row">
+        	<div class="col-xs-12 col-sm-8">
+                    <h3>هاکا گلوبال چیست؟</h3>
+                    <p class="text-justify">یک نبرد بین المللی برنامه نویسی جهت هم افزایی و شبکه سازی در بین جامعه برنامه نویسان در کل دنیاست، که تاکنون در چندین شهر دنیا برگزار شده است. تعداد هاکاسیتی ها در حال افزایش است و در آینده‌ی نردیک نیز افرادی از هر هاکاسیتی  در یک هاکاتون جهانی به نام هاکاگلوبال شرکت میکنند که در یک شهر توریستی برگزار میشود.</p>
+                
+            
+                    <h3>هدف برگزاری هاکا سیتی چیست؟</h3>
+                    <p class="text-justify">جامعۀ برنامه نویسان در دنیا خیلی فعال هستند و با اشتراک گذاری دانش میان یکدیگر باعث رشد و پیشرفت جامعۀ برنامه نویسی می شوند. در ایران کمی اوضاع فرق می کند و برنامه نویسان معمولاً زیاد از فعالیت های یکدیگر اطلاعی ندارند و در اتاق خودشان مشغول کدنویسی هستند. این رویداد باعث می شود که اولاً با همکاران خودشان بیشتر آشنا شوند و شبکه سازی در میان آنان به صورت حضوری و مجازی انجام شود، دوم اینکه شرکت هایی که به دنبال نیروهای متخصص می گردند، با حمایت مالی از رویداد می توانند برنامه نویسان خوب و خبره را در این رویدادها شناسایی و جذب نمایند.</p>
+              
+        	</div>
+            <div class="col-xs-12 col-sm-4">
+            	<img src="assets/images/rooydad.png" class="img-responsive center-block  hidden-xs" />
+            </div>
+    </div>
+    
+    <div class="container">
+      
+    </div>
+</section>
+
+
+</div><!-- /.wrapper -->
+
+
+<section id="next-event" class="section-next-event">
+	<h1 id="title" class="text-center">رویداد بعدی</h1>
+	<div class="container">
+    	<div class="col-xs-12 col-sm-5">
+            <img class="img-responsive img-thumbnail img-rounded center-block" src="assets/banner/rooydad.png" />
+        </div>
+        <div class="col-xs-12 col-sm-7 rtl">
+            <h2>رویداد هاکا گلوبال</h2>
+            <p class="text-justify">جامعۀ برنامه نویسان در دنیا خیلی فعال هستند و با اشتراک گذاری دانش میان یکدیگر باعث رشد و پیشرفت جامعۀ برنامه نویسی می شوند. در ایران کمی اوضاع فرق می کند و برنامه نویسان معمولاً زیاد از فعالیت های یکدیگر اطلاعی ندارند</p>
+            <div class="clock"></div>
+
+        </div>
+    </div>
+</section>
+
+
+<section id="support" class="section-support">
+	<h1 id="title" class="text-center">حامیان این رویداد</h1>
+    <div class="container text-center">
+    	<img src="assets/support/iea.png" />
+    	<img src="assets/support/iwf.png" />
+    	<img src="assets/support/roshanshow.png" />
+    	<img src="assets/support/startupweekend.png" />
+    </div>
+</section>
+
+
+
+
+<section id="footer" class="section-footer">
+	<div class="container">
+        <div class="row">
+        	<div class="col-xs-4 text-center">
+            	<img src="assets/logo/ahora_logo.png" width="100" />
+            </div>
+            <div class="col-xs-4">
+            	<p class="text-center">تمامی حقوق این سایت متعلق به هاکا سیتی می باشد</p>
+            	<p class="text-center">طراحی و توسعه توسط گروه اهورا</p>
+            </div>
+            <div class="col-xs-4 text-center">
+            	<img src="assets/images/logo_hacka_final.png" width="100" />
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<div class="modal fade" id="sign-up-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-right">ثبت نام</h4>
+      </div>
+      <div class="modal-body rtl">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label for="full_name" class="col-sm-3 control-label">نام و نام خانوادگی </label>
+            <div class="col-sm-9">
+              <input type="text" class="form-control" id="full_name" placeholder="نام و نام خانوادگی">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="email" class="col-sm-3 control-label">ایمیل</label>
+            <div class="col-sm-9">
+              <input type="email" class="form-control" id="email" placeholder="ایمیل">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="pass" class="col-sm-3 control-label">رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="password" class="form-control" id="pass" placeholder="رمز ورود">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="re-pass" class="col-sm-3 control-label">تکرار رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="re-pass" class="form-control" id="re-pass" placeholder="تکرار رمز ورود">
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer rtl">
+        <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
+        <button type="button" class="btn btn-success">ثبت نام</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+<div class="modal fade" id="login-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title text-right">ورود </h4>
+      </div>
+      <div class="modal-body rtl">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label for="email" class="col-sm-3 control-label">ایمیل</label>
+            <div class="col-sm-9">
+              <input type="email" class="form-control" id="email" placeholder="ایمیل">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="pass" class="col-sm-3 control-label">رمز ورود</label>
+            <div class="col-sm-9">
+              <input type="password" class="form-control" id="pass" placeholder="رمز ورود">
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer rtl">
+        <button type="button" class="btn btn-default" data-dismiss="modal">بستن</button>
+        <button type="button" class="btn btn-primary">ورود </button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+
+
+</body>
+		<script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script type="text/javascript" src="js/respond.js"></script>
+        <script type="text/javascript" src="js/parallax.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+        <script type="text/javascript" src="js/flipclock.js"></script>
+        <script type="text/javascript">
+		var clock;
+		
+		$(document).ready(function() {
+			var clock;
+
+			clock = $('.clock').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+				language: 'ar',
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });
+				    
+		    clock.setTime(220880);
+		    clock.setCountdown(true);
+		    clock.start();
+
+		});
+	</script>
+</html>
